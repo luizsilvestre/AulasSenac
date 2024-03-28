ac136bf0ab88ad2704232596a64e5bbc3e37c2f0
Luiz Silvestre Schwartz <120575172+luizsilvestre@users.noreply.github.com>
2024-03-27 21:28:40 -0300

    Add files via upload

---
+++ VariaveiseTipos/src/EntradanasFestas2.java
@@ -0,0 +1,18 @@
+
+public class EntradanasFestas2 {
+
+	public static void main(String[] args) {
+	
+		
+		int idade = 25;
+		
+		if ( idade <= 18 ) {
+			System.out.println("Não pode entrar");
+		}
+		else {
+			System.out.println("Seja bem vindo ");
+		}
+
+	}
+
+}
