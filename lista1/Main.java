4b6a8cd97ef9f5a662a7e0242fa76de4a2df3775
Luiz Silvestre Schwartz <120575172+luizsilvestre@users.noreply.github.com>
2024-04-04 20:41:30 -0300

    Add files via upload

---
+++ lista1/Main.java
@@ -0,0 +1,35 @@
+package br.senac.list01;
+
+public class Main {
+
+	public static void main(String[] args) {
+	//laMundo();
+/recebeUmNumero(10);
+	ImprimeSoma(10,30);
+	ImprimeMedia(6,7,8,9);
+	
+	
+	}
+
+	private static void ImprimeMedia() {
+		System.out.println(" A media é " + ((int i +int j +int k +int l)/4));
+		
+	}
+
+	private static void ImprimeSoma() {
+		System.out.println("O valor da soma é " +(int i +int j));
+	}
+
+	private static void recebeUmNumero(int numero) {
+		
+		numero =10;
+		System.out.println(" O numero informado foi " + numero);
+	}
+
+	private static void olaMundo() {
+		System.out.println("Ola mUndo");
+		
+	}
+	
+
+}
